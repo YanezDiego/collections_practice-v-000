a4bd2236148ea05dee140fcb2d66d2eb2184cd3c
Diego Yanez <diegosyanez92@gmail.com>
2018-04-03 15:39:29 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -29,11 +29,11 @@ def sort_array_char_count(array)
 end
 
 def swap_elements (array)
-  array[1], array[2] = array[2], array[1]
-array
+    array[1], array[2] = array[2], array[1]
+  array
 end
 
 def reverse_array (array)
-  array[0], array[2] = array[2], array [0]
-array
+    array[0], array[2] = array[2], array [0]
+  array
 end
