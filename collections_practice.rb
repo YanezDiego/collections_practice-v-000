586f0d19b410d8f81b8aecf373e340dcd8dbe484
Diego Yanez <diegosyanez92@gmail.com>
2018-04-03 15:33:06 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
+def sort_array_asc(number)
 
+  number.sort do |a, b|
+    a<=>b
+  end
+
+end
+
+def sort_array_desc(number)
+
+  number.sort do |a, b|
+    b<=>a
+  end
+
+end
+
+def sort_array_char_count(array)
+
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a.length < b.length
+      -1
+    else
+      1
+
+    end
+  end
+end
+
+def swap_elements (array)
+  array[1], array[2] = array[2], array[1]
+
+end
